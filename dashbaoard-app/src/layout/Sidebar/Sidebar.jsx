5321de1bc80018c7c0928c8cff05f09241724752
Chirag Paul <chiragpaul6474@gmail.com>
2025-07-10 23:02:55 +0530

    Added and modified the sidebar and its feature styling

--- dashbaoard-app/src/layout/Sidebar/Sidebar.jsx
+++ dashbaoard-app/src/layout/Sidebar/Sidebar.jsx
@@ -1,13 +1,28 @@
 import "./Sidebar.css";
 import { personsImgs } from "../../utils/images";
 import { navigationLinks } from "../../data/data";
-
+import { useContext, useEffect, useState } from "react";
+import { SidebarContext } from "../../context/sidebarContext";
 const Sidebar = () => {
+  const [activeLinkIdx] = useState(1);
+  const [sidebarClass, setSidebarClass] = useState("");
+  const { isSidebarOpen } = useContext(SidebarContext);
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      setSidebarClass("sidebar-change");
+    } else {
+      setSidebarClass("");
+    }
+  }, [isSidebarOpen]);
+
   return (
-    <div className={`sidebar`}>
+    <div className={`sidebar ${sidebarClass}`}>
       <div className="user-info">
-        <div className="info-img img -fit-cover">
-          <img src={personsImgs.person_two} alt="profile image" />
+        <div className="image-wrapper">
+          <div className="info-img img -fit-cover">
+            <img src={personsImgs.person_two} alt="profile image" />
+          </div>
         </div>
         <span className="info-name">alice-doe</span>
       </div>
