3111667e98d38a608d55001a91be2c04a860a2e7
Chirag Paul <chiragpaul6474@gmail.com>
2025-07-20 22:53:20 +0530

    Base Grid Of Budget has been created

---
+++ dashbaoard-app/src/components/Budget/Budget.jsx
@@ -0,0 +1,14 @@
+import "./Budget.css";
+import { iconsImgs } from "../../utils/images";
+import { budget } from "../../data/data";
+const Budget = () => {
+  return (
+    <div className="grid-two-item grid-common grid-c4">
+      <div className="grid-c-title">
+        <h3 className="grid-c-title-text">Budget</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Budget;
